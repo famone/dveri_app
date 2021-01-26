d756fbc5ca5e7c056fb1374308e3f2b5a6ffa21b
i9orek <ihar.kliuchnik.apps@gmail.com>
2021-01-26 16:42:19 +0300

    installation shedule/1

--- src/pages/InstallationSchedule.vue
+++ src/pages/InstallationSchedule.vue
@@ -5,27 +5,51 @@
     :loading="loading"
     single-line
     must-sort
-    class="elevation-1 rounded-lg"
-    sort-by="name"
-    group-by="category"
-    show-group-by
+    class="elevation-1 rounded-lg ma-4"
   >
+    <template #top>
+      <v-container fluid>
+        <v-row>
+          <v-spacer></v-spacer>
+          <v-col cols="3">
+            <v-select
+              placeholder="Выберите бригаду"
+              :items="teams"
+              v-model="title"
+              item-text="name"
+              return-object
+            ></v-select>
+          </v-col>
+        </v-row>
+      </v-container>
+    </template>
   </v-data-table>
 </template>
 
 <script>
+import axios from "axios";
 import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "InstallationSchedule",
 
+  data() {
+    return {
+      teams: [],
+      team: null,
+      headers: [
+        { text: "Адрес", value: "adress" },
+        { text: "Телефон", value: "phone" },
+        { text: "Модель двери", value: "door_model" },
+        { text: "Размер / Сторона", value: "door_size" },
+      ],
+    };
+  },
+
   computed: {
     ...mapGetters({
       sales: "zakaz/GET_SALES",
     }),
-    headers() {
-      return [];
-    },
   },
 
   methods: {
@@ -36,6 +60,14 @@ export default {
 
   async created() {
     await this.fetchDoors();
+    await axios
+      .get("https://door.webink.site/wp-json/door/v1/get/teams")
+      .then((response) => {
+        this.teams = response.data;
+      });
+    // let teams;
+
+    this.sales.forEach((sale) => {});
   },
 };
 </script>
